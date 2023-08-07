1004943e2b63dd3133caa35ddd32f68e58dde54e
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-08-07 21:56:52 +0200

    Add more commands

--- AboutMePlugin/src/main/kotlin/dns/mods/AboutMePlugin.kt
+++ AboutMePlugin/src/main/kotlin/dns/mods/AboutMePlugin.kt
@@ -9,44 +9,83 @@ class AboutMePlugin : JavaPlugin() {
 
   override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 
+    if (!(sender is Player)) {
+      return false
+    }
+
     when (label) {
       "myname" -> {
-        if (sender is Player) {
-          val myName = sender.displayName
-          sender.sendMessage("Hola Markus y Palle, mi usuario es ${myName}")
-          return true
-        }
+        val myName = sender.displayName
+        sender.sendMessage("Hola Markus y Palle, mi usuario es ${myName}")
+        return true
       }
 
       "mystats" -> {
-        if (sender is Player) {
-          sender.sendMessage("Food level: ${sender.foodLevel}")
-          sender.sendMessage("Health: ${sender.health}")
-          return true
-        }
+        sender.sendMessage("Food level: ${sender.foodLevel}")
+        sender.sendMessage("Health: ${sender.health}")
+        return true
       }
 
       "heal" -> {
-        if (sender is Player) {
-          sender.health = 20.0
-          sender.foodLevel = 20
-          sender.sendMessage("Tienes energia llena!!")
-          return true
-        }
+        sender.health = 20.0
+        sender.foodLevel = 20
+        sender.sendMessage("Tienes energia llena!!")
+        return true
       }
 
       "vacio" -> {
-        if (sender is Player) {
-          sender.health = 7.0
-          sender.foodLevel = 7
-          sender.sendMessage("Tienes energia vacia!!")
-          return true
-        }
+        sender.health = 7.0
+        sender.foodLevel = 7
+        sender.sendMessage("Tienes energia vacia!!")
+        return true
+      }
+
+      "mylocation" -> {
+        val loc = sender.location
+        sender.sendMessage("My location is x=${loc.x}, y=${loc.y}, z=${loc.z}")
+        return true
+      }
+
+      "jump" -> {
+        val loc = sender.location
+        loc.y += 20
+        sender.teleport(loc)
+        return true
+      }
+
+      "fast-speed" -> {
+        logger.info("Before walkspeed=${sender.walkSpeed}")
+        sender.walkSpeed = 1f
+        logger.info("After walkspeed=${sender.walkSpeed}")
+        return true
+      }
+
+      "normal-speed" -> {
+        logger.info("Before walkspeed=${sender.walkSpeed}")
+        sender.walkSpeed = 0.2f
+        logger.info("After walkspeed=${sender.walkSpeed}")
+        return true
+      }
+
+      "slow-speed" -> {
+        logger.info("Before walkspeed=${sender.walkSpeed}")
+        sender.walkSpeed = -1f
+        logger.info("After walkspeed=${sender.walkSpeed}")
+        return true
+      }
+
+      "level-up" -> {
+        sender.level += 1
+      }
+
+      "level-down" -> {
+        sender.level -= 1
+      }
+
+      "fire-ticks" -> {
+        sender.fireTicks = 60
       }
     }
-
-
-
 
     return false
   }
