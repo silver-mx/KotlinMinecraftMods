71da46c56c44aefac6021318a59fdaa29790c5b9
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-10-06 21:52:30 +0200

    Add command to teleport to last death location

--- AboutMePlugin/src/main/kotlin/dns/mods/AboutMePlugin.kt
+++ AboutMePlugin/src/main/kotlin/dns/mods/AboutMePlugin.kt
@@ -7,123 +7,136 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
+import java.util.Objects.nonNull
 
 class AboutMePlugin : JavaPlugin() {
 
-    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+  override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 
-        if (sender !is Player) {
-            return false
-        }
-
-        when (label) {
-            "myname" -> {
-                val myName = sender.displayName
-                sender.sendMessage("Hola Markus y Palle, mi usuario es ${myName}")
-                return true
-            }
-
-            "mystats" -> {
-                sender.sendMessage("Food level: ${sender.foodLevel}")
-                sender.sendMessage("Health: ${sender.health}")
-                return true
-            }
-
-            "heal" -> {
-                sender.health = sender.getAttribute(GENERIC_MAX_HEALTH)?.baseValue ?: 20.0
-                sender.foodLevel = 20
-                sender.sendMessage("Tienes energia llena!!")
-                return true
-            }
-
-            "armor" -> {
-                sender.inventory.helmet = ItemStack(Material.NETHERITE_HELMET)
-                sender.inventory.boots = ItemStack(Material.NETHERITE_BOOTS)
-                sender.inventory.chestplate = ItemStack(Material.NETHERITE_CHESTPLATE)
-                sender.inventory.leggings = ItemStack(Material.NETHERITE_LEGGINGS)
-
-                val sword = ItemStack(Material.NETHERITE_SWORD)
-                sender.inventory.addItem(sword)
-                return true
-            }
-
-            "hearts" -> {
-                val numHearts = args.first().toDouble() * 2
-                val attribute = sender.getAttribute(GENERIC_MAX_HEALTH)
-                attribute?.baseValue = numHearts
-
-                return true
-            }
-
-            "absortion" -> {
-                try {
-                    val absortionHearts = args.first().toDouble() * 2
-                    sender.absorptionAmount = absortionHearts
-                } catch (e: Exception) {
-                    logger.severe("Error executing absortion command: ${e.message}")
-                    sender.sendMessage("Error: ${e.message}")
-                }
-                return true
-            }
-
-            "vacio" -> {
-                sender.health = 7.0
-                sender.foodLevel = 7
-                sender.sendMessage("Tienes energia vacia!!")
-                return true
-            }
-
-            "mylocation" -> {
-                val loc = sender.location
-                sender.sendMessage("My location is x=${loc.x}, y=${loc.y}, z=${loc.z}")
-                return true
-            }
-
-            "jump" -> {
-                val loc = sender.location
-                loc.y += 20
-                sender.teleport(loc)
-                return true
-            }
-
-            "faster-speed" -> {
-                logger.info("Before walkspeed=${sender.walkSpeed}")
-                if (sender.walkSpeed + 0.2f <= 1.0f) {
-                    sender.walkSpeed += 0.2f
-                }
-                logger.info("After walkspeed=${sender.walkSpeed}")
-                return true
-            }
-
-            "normal-speed" -> {
-                logger.info("Before walkspeed=${sender.walkSpeed}")
-                sender.walkSpeed = 0.2f
-                logger.info("After walkspeed=${sender.walkSpeed}")
-                return true
-            }
-
-            "slower-speed" -> {
-                logger.info("Before walkspeed=${sender.walkSpeed}")
-                if (sender.walkSpeed - 0.2f >= -1f) {
-                    sender.walkSpeed -= 0.2f
-                }
-                logger.info("After walkspeed=${sender.walkSpeed}")
-                return true
-            }
-
-            "level-up" -> {
-                sender.level += 1
-            }
-
-            "level-down" -> {
-                sender.level -= 1
-            }
-
-            "fire-ticks" -> {
-                sender.fireTicks = 60
-            }
-        }
-
-        return false
+    if (sender !is Player) {
+      return false
     }
+
+    when (label) {
+      "myname" -> {
+        val myName = sender.displayName
+        sender.sendMessage("Hola Markus y Palle, mi usuario es ${myName}")
+        return true
+      }
+
+      "mystats" -> {
+        sender.sendMessage("Food level: ${sender.foodLevel}")
+        sender.sendMessage("Health: ${sender.health}")
+        return true
+      }
+
+      "heal" -> {
+        sender.health = sender.getAttribute(GENERIC_MAX_HEALTH)?.baseValue ?: 20.0
+        sender.foodLevel = 20
+        sender.sendMessage("Tienes energia llena!!")
+        return true
+      }
+
+      "armor" -> {
+        sender.inventory.helmet = ItemStack(Material.NETHERITE_HELMET)
+        sender.inventory.boots = ItemStack(Material.NETHERITE_BOOTS)
+        sender.inventory.chestplate = ItemStack(Material.NETHERITE_CHESTPLATE)
+        sender.inventory.leggings = ItemStack(Material.NETHERITE_LEGGINGS)
+
+        val sword = ItemStack(Material.NETHERITE_SWORD)
+        sender.inventory.addItem(sword)
+        return true
+      }
+
+      "hearts" -> {
+        val numHearts = args.first().toDouble() * 2
+        val attribute = sender.getAttribute(GENERIC_MAX_HEALTH)
+        attribute?.baseValue = numHearts
+
+        return true
+      }
+
+      "absortion" -> {
+        try {
+          val absortionHearts = args.first().toDouble() * 2
+          sender.absorptionAmount = absortionHearts
+        } catch (e: Exception) {
+          logger.severe("Error executing absortion command: ${e.message}")
+          sender.sendMessage("Error: ${e.message}")
+        }
+        return true
+      }
+
+      "vacio" -> {
+        sender.health = 7.0
+        sender.foodLevel = 7
+        sender.sendMessage("Tienes energia vacia!!")
+        return true
+      }
+
+      "mylocation" -> {
+        val loc = sender.location
+        sender.sendMessage("My location is x=${loc.x}, y=${loc.y}, z=${loc.z}")
+        return true
+      }
+
+      "jump" -> {
+        val loc = sender.location
+        loc.y += 20
+        sender.teleport(loc)
+        return true
+      }
+
+      "faster-speed" -> {
+        logger.info("Before walkspeed=${sender.walkSpeed}")
+        if (sender.walkSpeed + 0.2f <= 1.0f) {
+          sender.walkSpeed += 0.2f
+        }
+        logger.info("After walkspeed=${sender.walkSpeed}")
+        return true
+      }
+
+      "normal-speed" -> {
+        logger.info("Before walkspeed=${sender.walkSpeed}")
+        sender.walkSpeed = 0.2f
+        logger.info("After walkspeed=${sender.walkSpeed}")
+        return true
+      }
+
+      "slower-speed" -> {
+        logger.info("Before walkspeed=${sender.walkSpeed}")
+        if (sender.walkSpeed - 0.2f >= -1f) {
+          sender.walkSpeed -= 0.2f
+        }
+        logger.info("After walkspeed=${sender.walkSpeed}")
+        return true
+      }
+
+      "level-up" -> {
+        sender.level += 1
+        return true
+      }
+
+      "level-down" -> {
+        sender.level -= 1
+        return true
+      }
+
+      "fire-ticks" -> {
+        sender.fireTicks = 60
+        return true
+      }
+
+      "deathloc" -> {
+        if (nonNull(sender.lastDeathLocation)) {
+          sender.teleport(sender.lastDeathLocation!!)
+        }
+
+        return true
+      }
+    }
+
+    return false
+  }
 }
